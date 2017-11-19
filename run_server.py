acf06b3b786d843e2e91b59bb0f42894a9363c53
Rytis Karpuska <rytis.karpuska@gmail.com>
2017-11-19 20:34:29 +0200

    Use cascading in run_Server.py

--- run_server.py
+++ run_server.py
@@ -2,9 +2,14 @@
 
 import webapp2
 from main import app
+from paste.urlparser import StaticURLParser
+from paste.cascade import Cascade
 from paste import httpserver
 
-httpserver.serve(app, host="127.0.0.1", port="8080")
+static_app = StaticURLParser("html/")
+my_app = Cascade([static_app, app])
+
+httpserver.serve(my_app, host="127.0.0.1", port="8080")
 
 
 
