3daa0b042554d9ecf84fa999bfd8a972da57d8b0
Rytis Karpuska <rytis.karpuska@gmail.com>
2017-11-25 18:00:12 +0200

    First version of GUI.

---
+++ html/app.js
@@ -0,0 +1,103 @@
+
+var API_base = "HashReversing/JSON";
+var hash_types = ["SHA256", "SHA512"];
+
+var app = angular.module('HashReversing', ["ngRoute"]);
+app.controller('HashReversingController', function($scope, $timeout, $http, $route) {
+	$scope.menu_items = hash_types;
+	$scope.inputs = {};
+	$scope.inputs.hash_error = null;
+	$scope.inputs.value_error = null;
+
+	$scope.$on('$routeChangeStart', function($event, next, current) {
+		delete $scope.inputs.hash;
+		delete $scope.inputs.value;
+	});
+
+	$scope.on_value_change = function(new_value) {
+		if (typeof $scope.update_delayer != "undefined")
+			$timeout.cancel($scope.update_delayer);
+
+		if (new_value.length == 0)
+			return;
+
+		var current_hash_type = hash_types[0];
+		for (var idx in hash_types)
+		{
+			if ($route.current.templateUrl.indexOf(hash_types[idx]) !== -1)
+			{
+				current_hash_type = hash_types[idx];
+				break;
+			}
+		}
+
+		$scope.update_delayer = $timeout(function() {
+			$http.get(API_base + "/" + current_hash_type + "/digest", {
+				params: {value: new_value}
+			}).then(function(response) {
+				delete $scope.inputs.value_error;
+				delete $scope.inputs.hash_error;
+				$scope.inputs.hash = response.data.digest;
+			},
+			function (response) {
+				delete $scope.inputs.hash;
+				$scope.inputs.value_error = response.data.error;
+			}
+			)}, 500);
+	}
+
+	$scope.on_hash_change = function(new_hash) {
+		if (typeof $scope.update_delayer != "undefined")
+			$timeout.cancel($scope.update_delayer);
+
+		if (new_hash.length == 0)
+			return;
+
+		var current_hash_type = hash_types[0];
+		for (var idx in hash_types)
+		{
+			if ($route.current.templateUrl.indexOf(hash_types[idx]) !== -1)
+			{
+				current_hash_type = hash_types[idx];
+				break;
+			}
+		}
+
+		$scope.update_delayer = $timeout(function() {
+			$http.get(API_base + "/" + current_hash_type + "/value", {
+				params: {digest: new_hash}
+			}).then(function(response) {
+				delete $scope.inputs.value_error;
+				delete $scope.inputs.hash_error;
+				$scope.inputs.value = response.data.value;
+			},
+			function (response) {
+				delete $scope.inputs.value;
+				$scope.inputs.hash_error = response.data.error;
+			}
+			)}, 500);
+	}
+});
+
+app.config(function ($routeProvider) {
+
+	$routeProvider.when("/", {
+		templateUrl : hash_types[0] + ".html"
+	});
+
+	for (var idx in hash_types)
+	{
+		requested_URL = "/" + hash_types[idx];
+		required_URL = requested_URL + ".html";
+		$routeProvider.when(requested_URL, {
+			templateUrl : required_URL
+		});
+	}
+
+	$routeProvider.otherwise({
+		template : "<p>Unknown page requested</p>"
+	});
+
+});
+
+
